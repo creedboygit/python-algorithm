81677e65d95a0c8d1d45f98f7e8d2b5196bb1fd6
creedboy <polozhzh@gmail.com>
2024-05-02 15:22:35 +0900

    집합 자료구조

--- 100-python-basic/101-python-basic.py
+++ 100-python-basic/101-python-basic.py
@@ -51,7 +51,6 @@ print(a)
 a.pop(2)
 print(a)
 
-
 # insert 함수의 시간 복잡도는 O(N)_이므로, 남발하면 시간 초과날 수 있다.
 # 특정 값의 원소를 모두 제거하려면 remove_set = {3, 5} 에 제거할 원소를 넣은 후
 # result = [i for i in a if i not in remove_set] 으로 처리
@@ -97,12 +96,36 @@ print(list(key_list))
 value_list = price.values()
 print(list(value_list))
 
-if '사과' in price: # 사전 자료형에 특정한 원소가 있는지 검사 가능
+if '사과' in price:  # 사전 자료형에 특정한 원소가 있는지 검사 가능
     print("'사과'가 존재합니다.")
 
-if '풋사과' in price: # 사전 자료형에 특정한 원소가 있는지 검사 가능
+if '풋사과' in price:  # 사전 자료형에 특정한 원소가 있는지 검사 가능
     print("'풋사과'가 존재합니다.")
 else:
     print("'풋사과'가 존재하지 않습니다.")
+
+# 집합
+data = set([1, 1, 2, 3, 4, 4, 5])  # 중복이 제거됨 {1, 2, 3, 4, 5}
+print("===========")
+print(data)
+
+data = {1, 1, 2, 2, 3, 3, 3, 4, 5, 5}  # 중복이 제거됨 {1, 2, 3, 4, 5}
+print(data)
+
+data.add("6")
+print(data)
+
+data.add(7)
+print(data)
+
+data.update([8, 9, "10"])  # 여러 개의 원소 추가
+print(data)
+
+data.remove("10")  # 특정한 값을 갖는 원소 삭제
+print(data)
+
+# a | b # 합집합
+# a & b # 교집합
+# a - b # 차집합
 
 
