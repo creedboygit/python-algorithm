98edf71e9b23fd765794149f592160d34ff3415a
creedboy <polozhzh@gmail.com>
2024-05-27 14:05:41 +0900

    문자열 포맷팅 추가

--- 100-python-basic/102-python-basic.py
+++ 100-python-basic/102-python-basic.py
@@ -7,3 +7,28 @@ print(val[:2])
 print(val[-1])
 print(val[-3:])
 print(val[-4:-2])
+
+# 문자열 포맷팅
+sam = '샘'
+a = '안녕하세요 %s' % sam
+print("===========")
+print(a)
+
+today = 3
+b = '오늘은 %d 일입니다' % today
+print(b)
+
+pi = 3.14
+c = '파이는 %d ' % pi
+print(c)
+
+# d = '%d'
+d = '%d-%02d-%02d' % (2022, 4, 2)
+print("===========")
+print(d)
+
+f = '%.4f' % pi
+print(f)
+
+g = '여기는 {} {} {} 입니다'.format('대한민국', '서울시', '강남구')
+print(g)
