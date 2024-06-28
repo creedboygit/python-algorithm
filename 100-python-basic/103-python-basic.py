dadf530d2c39f5a58f0952f3986ec0549779ee96
creedboy <polozhzh@gmail.com>
2024-06-28 14:20:32 +0900

    bisect 추가

--- 100-python-basic/103-python-basic.py
+++ 100-python-basic/103-python-basic.py
@@ -520,3 +520,34 @@ def heapsort(iterable):
 result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
 print("===========")
 print(result)
+
+# bisect
+# 이진 탐색을 쉽게 구현할 수 있도록 하는 라이브러리
+# 정렬된 배열에서 특정한 원소를 찾아야할 때 매우 효과적으로 사용
+# bisect_left(a, x) - 정렬된 순서를 유지하면서 리스트 a에 데이터 x를 삽입할 가장 왼쪽 인덱스를 찾는 메서드
+# bisect_right(a, x) - 정렬된 순서를 유지하면서 리스트 a에 데이터 x를 삽입할 가장 오른쪽 인덱스를 찾는 메서드
+# 정렬된 리스트에서 값이 특정 범위에 속하는 원소의 개수를 구하고자 할 때 사용
+from icecream import ic
+
+from bisect import bisect_left, bisect_right
+
+a = [1, 2, 4, 4, 8]
+x = 4
+
+ic(bisect_left(a, x))
+ic(bisect_right(a, x))
+
+# 정렬된 리스트에서 값이 특정 범위에 속하는 원소의 개수를 구하고자 할 때 사용
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
+
+# 리스트 선언
+a = [1, 2, 3, 3, 3, 3, 4, 4, 8, 9]
+
+# 값이 4인 데이터 개수 출력
+ic(count_by_range(a, 4, 4))
+
+# 값이 [01, 3] 범위에 있는 데이터 개수 출력
+ic(count_by_range(a, -1, 3))
